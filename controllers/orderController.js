4f67969a4175b4743fa6e69507066ab11d0e84ed
Vishwajeet Srivastava <vs0397297@gmail.com>
2026-06-22 17:50:23 +0530

    Added new features and fixes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModels.js"
+import Stripe from "stripe"
+
+//global variable
+const currency = "inr"
+const deliveryCharge = 10
+
+// gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 // Placing order using COD Method
@@ -31,7 +39,91 @@ const placeOrder = async (req,res) => {
 
 // Placing order using Stripe Method
 const placeOrderStripe = async (req,res) => {
-   
+   try {
+       const {userId, items, amount, address} = req.body
+       const {origin} = req.headers
+
+       const orderData = {
+         userId, 
+         items,
+         amount, 
+         address,
+         paymentMethod: "Strip",
+         payment: false,
+         date: Date.now()
+       }
+
+       const newOrder = new orderModel(orderData)
+       await newOrder.save()
+
+       const line_items = items.map((item) => ({
+         price_data: {
+            currency:currency,
+            product_data: {
+               name: item.name
+            },
+            unit_amount: item.price * 100
+         },
+         quantity:item.quantity
+       }))
+
+       line_items.push({
+         price_data: {
+            currency:currency,
+            product_data: {
+               name: "Delivery Charges"
+            },
+            unit_amount:  deliveryCharge * 100
+         },
+         quantity: 1
+       })
+
+       const session = await stripe.checkout.sessions.create({
+         success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+         cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+         line_items,
+         mode:"payment",
+       })
+        res.json({success:true, session_url:session.url})
+   } catch (error) {
+      console.log(error);
+      res.json({success:false, message:error.message})
+      
+   }
+}
+
+// Verify Stripe
+
+const verifyStripe = async (req,res) => {
+   try {
+
+      const { success, orderId } = req.body;
+
+      if(success === "true"){
+
+         const order = await orderModel.findById(orderId);
+
+         await orderModel.findByIdAndUpdate(orderId, {
+            payment: true
+         });
+
+         await userModel.findByIdAndUpdate(order.userId, {
+            cartData: {}
+         });
+
+         res.json({success:true});
+
+      }else{
+
+         await orderModel.findByIdAndDelete(orderId);
+         res.json({success:false});
+
+      }
+
+   } catch (error) {
+      console.log(error);
+      res.json({success:false,message:error.message});
+   }
 }
 
 // Placing order using Razorpay Method
@@ -41,17 +133,40 @@ const placeOrderRazorpay = async (req,res) => {
 
 // All Orders data for Admin Panel
 const allOrders = async (req,res) => {
-   
+   try {
+
+    const orders = await orderModel.find({})
+    res.json({success:true,orders})
+
+   } catch (error) {
+       console.log(error)
+       res.json({success:false, message:error.message})
+   }
 }
 
 // User Order Data for Frontend
 const userOrders = async (req,res) => {
-   
+   try {
+    const {userId} = req.body
+
+    const orders = await orderModel.find({userId})
+    res.json({success:true,orders})
+   } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message})
+   }
 }
 
 // update order status from Admin Panel
 const updateStatus = async (req,res) => {
-   
+   try {
+      const {orderId, status} = req.body
+      await orderModel.findByIdAndUpdate(orderId, {status})
+      res.json({success:true, message: "Status Updated"})
+   } catch (error) {
+      console.log(error);
+      res.json({success:false,message:error.message})
+   }
 }
 
-export {placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus}
+export {verifyStripe, placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus}
