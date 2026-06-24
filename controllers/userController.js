f694ea3f64510f2cf7a8d3bc82dff4f0d5469a9e
Vishwajeet Srivastava <vs0397297@gmail.com>
2026-06-24 18:50:12 +0530

    Fix admin login env variable

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,7 +90,7 @@ const registerUser = async (req,res) => {
 const adminLogin = async (req,res) => {
     try {
          const {email,password} = req.body
-    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_Password){
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
         const token = jwt.sign(email+password,process.env.JWT_SECRET)
         res.json({success:true,token})
     }else{
